3ad8f1369793c1bc1a651972c15a4846359ed1c0
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-27 01:27:37 -0300

    Inserir imagem para o ultimo projeto nextjs.

--- src/data/mainProjects.js
+++ src/data/mainProjects.js
@@ -19,7 +19,7 @@ export let mainProjects = [
 		title: 'Next-JS Weather App',
 		urlRepository: 'https://github.com/ahoymarcus/nextjs-weather-app',
 		urlApp: 'https://nextjs-weather-app-dun.vercel.app/',
-		image: '/images/frontend/',
+		image: '/images/frontend/nextjs-weather-app-01.png',
 		description: 'Um app Next.JS que renderiza informações do tempo de cidades ao redor do mundo utilizando a API Open Weather (https://openweathermap.org/), passando alguns parâmetros definir o retorno dos dados.',
 		tags: ['react-js', 'next-js', 'api rest', 'scss', 'moment-timezone'],
 	},

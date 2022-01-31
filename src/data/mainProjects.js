d072ec2c47abfb3be22181acd704d364d3fcc122
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-31 13:40:34 -0300

    Inserir segunda imagem para o projeto my JS news blog. Alterar nos dados dos projetos.

--- src/data/mainProjects.js
+++ src/data/mainProjects.js
@@ -3,7 +3,7 @@ export let mainProjects = [
 		title: 'Next-JS JSnews Blog',
 		urlRepository: 'https://github.com/ahoymarcus/nextjs-myjs-tech-blog',
 		urlApp: 'https://nextjs-myjs-tech-blog.vercel.app/',
-		image: '/images/frontend/nextjs-myjs-tech-blog-01.png',
+		image: '/images/frontend/nextjs-myjs-tech-blog-02.png',
 		description: 'My personal blog with articles and informations about JavaScript and Web Development.',
 		tags: ['react-js', 'next-js', 'gray-matter', 'remark', 'remark-html', 'date-fns'],
 	},{

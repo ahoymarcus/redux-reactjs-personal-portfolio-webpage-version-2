8ea5197ce82eade813361d0b429713a33a545e62
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-31 01:08:20 -0300

    Inserir projeto my JS news blog.

--- src/data/mainProjects.js
+++ src/data/mainProjects.js
@@ -1,5 +1,12 @@
 export let mainProjects = [
 	{
+		title: 'Next-JS JSnews Blog',
+		urlRepository: 'https://github.com/ahoymarcus/nextjs-myjs-tech-blog',
+		urlApp: 'https://nextjs-myjs-tech-blog.vercel.app/',
+		image: '/images/frontend/nextjs-myjs-tech-blog-01.png',
+		description: 'My personal blog with articles and informations about JavaScript and Web Development.',
+		tags: ['react-js', 'next-js', 'gray-matter', 'remark', 'remark-html', 'date-fns'],
+	},{
 		title: 'Rocket Launches App',
 		urlRepository: 'https://github.com/ahoymarcus/reactjs-rocket-launches-app',
 		urlApp: 'https://reactjs-rocket-launches-app.netlify.app/',
--- src/data/projects-data-schema.js
+++ src/data/projects-data-schema.js
@@ -1,11 +1,11 @@
 export let projectsDataSchema = [
 	{
-		title: 'Next-JS Weather App',
-		urlRepository: 'https://github.com/ahoymarcus/nextjs-weather-app',
-		urlApp: 'https://nextjs-weather-app-dun.vercel.app/',
-		image: '/images/frontend/',
-		description: 'Um app Next.JS que renderiza informações do tempo de cidades ao redor do mundo utilizando a API Open Weather (https://openweathermap.org/), passando alguns parâmetros definir o retorno dos dados.',
-		tags: ['react-js', 'next-js', 'api rest', 'scss', 'moment-timezone'],
+		title: 'Next-JS JSnews Blog',
+		urlRepository: 'https://github.com/ahoymarcus/nextjs-myjs-tech-blog',
+		urlApp: 'https://nextjs-myjs-tech-blog.vercel.app/',
+		image: '/images/frontend/nextjs-myjs-tech-blog-01.png',
+		description: 'My personal blog with articles and informations about JavaScript and Web Development.',
+		tags: ['react-js', 'next-js', 'gray-matter', 'remark', 'remark-html', 'date-fns'],
 	},
 ];
 

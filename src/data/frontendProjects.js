3cf40e99ee765bcdc27904cdd9ff7b6c8c054728
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-06-23 14:11:20 -0300

    Update javascript calculator project metadata tags

--- src/data/frontendProjects.js
+++ src/data/frontendProjects.js
@@ -235,7 +235,7 @@ export let frontendProjects = [
 		urlApp: 'https://ahoymarcus.github.io/odin-calculator-projetct/',
 		image: '/images/frontend/odin-javascript-calculator-project-01.png',
 		description: 'This is the fifth project from The Odin Project and the third one from the JavaScript section. It aims to create a JavaScript web calculator app.',
-		tags: ['html', 'css', 'css-patterns', 'javascript'],
+		tags: ['html', 'css', 'css-patterns', 'javascript', 'regex'],
 	},
     {
 		title: 'Rock Paper & Scissors App',
--- src/data/mainProjects.js
+++ src/data/mainProjects.js
@@ -84,7 +84,7 @@ export let mainProjects = [
 		urlApp: 'https://ahoymarcus.github.io/odin-calculator-projetct/',
 		image: '/images/frontend/odin-javascript-calculator-project-01.png',
 		description: 'This is the fifth project from The Odin Project and the third one from the JavaScript section. It aims to create a JavaScript web calculator app.',
-		tags: ['html', 'css', 'css-patterns', 'javascript'],
+		tags: ['html', 'css', 'css-patterns', 'javascript', 'regex'],
 	},
 	{
 		title: 'Music Database - express-js',

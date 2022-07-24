f1c18e794aa7c1a7a7fa94ee9c1ba26cd0ef60ad
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-07-24 12:09:12 -0300

    Fix typos

--- src/data/vanillaJSProjects.js
+++ src/data/vanillaJSProjects.js
@@ -4,7 +4,7 @@ export let vanillaJSProjects = [
 		urlRepository: 'https://github.com/ahoymarcus/my-javascript-bookshelf',
 		urlApp: 'https://my-javascript-bookshelf.netlify.app/',
 		image: '/images/frontend/myjavascript-bookshelf-01.png',
-		description: 'My personal project to build book library entiriry with JavaScript. In the first part of the endevour, it started with the UML modeling and designing the Wireframe and the Layouts with Figma. And it has the basic UI and functionalities ready',
+		description: 'My personal project for building a book library entirely with JavaScript. In this first part, it starts with the UML modeling of core features and designing the Wireframe and the Layout pages with Figma framework. Besides, it has the basic UI and functionalities ready.',
 		tags: ['html', 'css', 'css-patterns', 'javascript', 'js forms validation', 'Figma'],
 	},
 	{
